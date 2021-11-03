1bb1b07fc67e11be7ff3de6c0fd1ae467ef24139
Stelin <stelinduarte100@gmail.com>
2021-11-03 11:31:22 -0600

    :rocket: Webpack + Typescript finished

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React, { ReactElement } from "react";
+import image from "./camiseta.png";
+import logo from "./react.svg";
+
+interface Props {}
+
+export default function App({}: Props): ReactElement {
+  return (
+    <>
+      <h1>React TypeScript Template! </h1>
+    </>
+  );
+}
